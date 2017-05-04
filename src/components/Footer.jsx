36c92f6475086456a754bb3e692bcba0213dcbb1
Giangnt <nguyentruonggiang71096@gmail.com>
2017-05-05 00:11:05 +0700

    123

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class Footer extends React.Component {
-  render() {
-    return <footer className="info">
-      <p>Written by hungnds</p>
-    </footer>
-  }
-};
